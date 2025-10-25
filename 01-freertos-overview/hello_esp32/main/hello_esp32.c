feb8b1086ae0df7d4617fd9d385b827e6df311c2
phuwishpk <phuwish123@gmail.com>
2025-10-25 15:45:05 +0700

    commitlab2

--- 01-freertos-overview/hello_esp32/main/hello_esp32.c
+++ 01-freertos-overview/hello_esp32/main/hello_esp32.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_system.h"
+#include "build_info.h"
+
 
 void app_main(void)
 {
-    printf("Hello, ESP32 World!\n");
+    printf("=== %s v%s ===\n", PROJECT_NAME, PROJECT_VERSION);
+    printf("Built on: %s %s\n", BUILD_DATE, BUILD_TIME);
+    printf("ESP-IDF Version: %s\n", esp_get_idf_version());
+    printf("Chip: %s\n", CONFIG_IDF_TARGET);
+    printf("Free heap: %d bytes\n", esp_get_free_heap_size());
     
-    // Keep the program running
     while(1) {
-        printf("ESP32 is running...\n");
-        vTaskDelay(pdMS_TO_TICKS(1000)); // 1 second delay
+        printf("System is running...\n");
+        vTaskDelay(pdMS_TO_TICKS(2000));
     }
 }
